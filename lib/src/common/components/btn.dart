ddc19dfa64c64f075b1069f99827c98fd16c1b1a
work <qkqhemforhs1@gmail.com>
2023-11-18 15:19:51 +0900

    회원가입페이지
    UI 개발

--- lib/src/common/components/btn.dart
+++ lib/src/common/components/btn.dart
@@ -5,8 +5,9 @@ class Btn extends StatelessWidget {
 
   final Function() onTap;
   final String text;
+  final Color? backgroudColor;
 
-  const Btn({super.key, required this.onTap,required this.text});
+  const Btn({super.key, required this.onTap,required this.text,this.backgroudColor});
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +17,7 @@ class Btn extends StatelessWidget {
         padding: const EdgeInsets.symmetric(vertical: 15),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(7),
-          color: const Color(0xffF4AA2B),
+          color: backgroudColor ?? const Color(0xffF4AA2B),
         ),
         child: AppFont(text,
             fontWeight: FontWeight.bold, textAlign: TextAlign.center),
--- lib/src/signup/page/signup_page.dart
+++ lib/src/signup/page/signup_page.dart
@@ -1,15 +1,158 @@
+import 'package:book1/src/common/components/app_font.dart';
+import 'package:book1/src/common/components/btn.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SignupPage extends StatelessWidget {
   const SignupPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(child: Text("회원가입 페이지")),
+      appBar: AppBar(actions: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 7, horizontal: 20),
+          child: GestureDetector(
+            onTap: () {},
+            child: SvgPicture.asset('assets/svg/icons/icon_close.svg'),
+          ),
+        )
+      ]),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            _UserProfileImageFiled(),
+            SizedBox(
+              height: 50,
+            ),
+            _NicknameFiled(),
+            SizedBox(
+              height: 30,
+            ),
+            _DiscriptionFiled(),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding:  EdgeInsets.only(left: 20,right: 20,bottom: 20+MediaQuery.of(context).padding.bottom,top: 20),
+        child: Row(
+          children: [
+            Expanded(
+              child: Btn(
+                onTap: () {},
+                text: '가입',
+              ),
+            ),
+            SizedBox(width: 10,),
+            Expanded(
+              child: Btn(
+                onTap: () {},
+                backgroudColor: Color(0xff212121),
+                text: '취소',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _UserProfileImageFiled extends StatelessWidget {
+  const _UserProfileImageFiled({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CircleAvatar(
+        backgroundColor: Colors.grey,
+        radius: 50,
+        backgroundImage: Image.asset('assets/images/default_avatar.png').image,
+      ),
+    );
+  }
+}
+
+class _NicknameFiled extends StatelessWidget {
+  const _NicknameFiled({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        AppFont(
+          '닉네임',
+          size: 16,
+          fontWeight: FontWeight.bold,
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        TextField(
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.white, fontSize: 18),
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: const Color(0xff232323),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(6),
+              gapPadding: 0,
+              borderSide: BorderSide.none,
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(6),
+              gapPadding: 0,
+              borderSide: BorderSide.none,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _DiscriptionFiled extends StatelessWidget {
+  const _DiscriptionFiled({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        AppFont(
+          '한줄 소개',
+          size: 16,
+          fontWeight: FontWeight.bold,
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        TextField(
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.white, fontSize: 12),
+          maxLength: 50,
+          decoration: InputDecoration(
+            counterStyle: TextStyle(
+              color: Colors.white,
+              fontSize: 15,
+            ),
+            filled: true,
+            fillColor: const Color(0xff232323),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(6),
+              gapPadding: 0,
+              borderSide: BorderSide.none,
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(6),
+              gapPadding: 0,
+              borderSide: BorderSide.none,
+            ),
+          ),
+        )
+      ],
     );
   }
 }
